a9f44450d3a3e5116a84c00eba7d1105d8d9926a
ErosLibidus <100135900+ErosLibidus@users.noreply.github.com>
2022-09-08 15:44:02 -0300

    add upload postgres (#9)

---
+++ datawarehouse/postgres_upload.py
@@ -0,0 +1,32 @@
+import psycopg2
+import sqlalchemy
+from sqlalchemy import create_engine
+import os
+import pandas as pd
+from decouple import config
+
+user = config("USER")
+passw = config("PASSWORD")
+host = config("HOST")
+database = config("DATABASE")
+
+def baseDeDatos (df, tabla):
+    try:
+        conex = create_engine("postgresql+psycopg2://{}:{}@{}/{}".format(user,passw,host,database))
+        conexpostgres = conex.connect()
+        metadatas = sqlalchemy.MetaData()
+        df.to_sql(tabla, conexpostgres, if_exists="append", index=False)
+        return "Tabla subida"
+    except Exception as ex:
+        return ex
+
+def upload ():
+    with os.scandir("./Normalizados") as ficheros:
+        n = 0
+        for i in ficheros:
+            dir= "./Normalizados/" + i.name
+            df = pd.read_csv(dir)
+            baseDeDatos(df, i.name[:-4])
+            n+=1
+            print("{} Archivos subidos a la base de datos".format(n))
+        print("Todos los archivos se subieron correctamente")

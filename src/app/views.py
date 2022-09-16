c4996d33c86aa21c36aadb2afe1e8284a2d09a30
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 <arodriguezv@uni.pe>
2022-09-15 21:41:19 -0500

    changes app main

--- src/app/views.py
+++ src/app/views.py
@@ -1,13 +1,4 @@
 from django.shortcuts import render
-from geospatial.main import  barPlot, linealPlot, emisionco2
 
 def home(request):
-    n = barPlot(),
-    m = linealPlot(),
-    g = emisionco2()
-    context={
-        'barPlot':n,
-        'linealPlot':m,
-        'emisionPlot':g
-    }
-    return render(request,'index.html',context)
+    return render(request,'index.html')

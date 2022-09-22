57182317bb4993d2e0694c8526931cb703778e16
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 <arodriguezv@uni.pe>
2022-09-22 00:57:03 -0500

    Add connection between graphics (#15)
    the connection of the timeline with the map and the independent world and post-country graphics was made

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -4,18 +4,6 @@ team.addEventListener('click',()=>{
     submenu.classList.toggle('show__team')
 })
 
-const aside__left = document.querySelector('.aside__left')
-const click__left__open = document.querySelector('#click__left__open')
-const click__left__close = document.querySelector('#click__left__close')
-
-click__left__open.addEventListener('click',()=>{
-    console.log('*********')
-    aside__left.classList.add('move__left')
-})
-click__left__close.addEventListener('click',()=>{
-    aside__left.classList.remove('move__left')
-})
-
 const aside__right = document.querySelector('.aside__right')
 const click__right__open = document.querySelector('#click__right__open')
 const click__right__close = document.querySelector('#click__right__close')

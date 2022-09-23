77c3902f0b6193a1bf1ef6673f36eb4379060e72
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 <arodriguezv@uni.pe>
2022-09-23 12:34:12 -0500

    Change models api (#22)

--- src/api/api.py
+++ src/api/api.py
@@ -17,6 +17,11 @@ from django_filters.rest_framework import DjangoFilterBackend
 
 
 class PaisConInformacionApiView(generics.ListAPIView):
+    """
+    Información General por país
+
+    En estructura anidada 
+    """
     serializer_class = PaisConInformacionSerializer
 
     def get_queryset(self):
@@ -25,28 +30,32 @@ class PaisConInformacionApiView(generics.ListAPIView):
 
 
 class HuellaPaisApiView(generics.ListAPIView):
-    serializer_class = HuellaPaisSerializer
-    # filter_backends = [DjangoFilterBackend, filters.SearchFilter]
-    # filterset_fields = {
-    #     'iso_code' : ['contains'],
-    #     'year': ['contains']
-    # }
+    """
+    KPI's histórico por país
 
-    # search_fields = ['country']
+    Principales métricas para cada pais según la ISO 3166
+    """
+    serializer_class = HuellaPaisSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ('codigo','continente')
+
+    search_fields = ['pais']
 
     def get_queryset(self):
         queryset = HuellaPais.objects.all()
         return queryset
 
 class HuellaMundialApiView(generics.ListAPIView):
-    serializer_class = HuellaMundialSerializer
-    # filter_backends = [DjangoFilterBackend, filters.SearchFilter]
-    # filterset_fields = {
-    #     'iso_code' : ['contains'],
-    #     'year': ['contains']
-    # }
+    """
+    Histórico mundial de KPI's medioambientales
 
-    # search_fields = ['country']
+    biocapacidad -> Representa la disponibilidad de superficie biológicamente productiva por habitante. Unidad: gha/persona (1 gha es la capacidad productiva de 1 ha de tierra de producción media mundial)
+
+    huella_ecológica -> Total de superficie ecológicamente productiva para producir los recursos consumidos por un habitante y los residuos que genera.  Unidad hgpc (hectárea global per Cópita: la capacidad productiva de 1 ha de tierra de producción media mundial)
+
+    tierra -> Cociente  entre (huella ecologica/biocapacidad) define la cantidad de  planetas Tierra que se necesita para sustentar a la población.
+    """
+    serializer_class = HuellaMundialSerializer
 
     def get_queryset(self):
         queryset = HuellaMundial.objects.all()
@@ -54,14 +63,13 @@ class HuellaMundialApiView(generics.ListAPIView):
 
 
 class GasesMundialApiView(generics.ListAPIView):
-    serializer_class = GasesMundialSerializer
-    # filter_backends = [DjangoFilterBackend, filters.SearchFilter]
-    # filterset_fields = {
-    #     'iso_code' : ['contains'],
-    #     'year': ['contains']
-    # }
+    """
+    Histórico mundial de gases del efecto invernadero
 
-    # search_fields = ['country']
+    Con unidades en MtCO₂e, N2O (Óxido Nitroso), SF6 (Hexafloruro de Azufre),
+    CO2 (Dióxido de Carbono), CH4 (Metano), anio (año)
+    """
+    serializer_class = GasesMundialSerializer
 
     def get_queryset(self):
         queryset = GasesMundial.objects.all()
@@ -69,14 +77,12 @@ class GasesMundialApiView(generics.ListAPIView):
 
 
 class TemperaturaMundialApiView(generics.ListAPIView):
-    serializer_class = TemperaturaMundialSerializer
-    # filter_backends = [DjangoFilterBackend, filters.SearchFilter]
-    # filterset_fields = {
-    #     'iso_code' : ['contains'],
-    #     'year': ['contains']
-    # }
+    """
+    Comportamiento histórico de la temperatura
 
-    # search_fields = ['country']
+    Medición global 
+    """
+    serializer_class = TemperaturaMundialSerializer
 
     def get_queryset(self):
         queryset = TemperaturaMundial.objects.all()
@@ -84,10 +90,6 @@ class TemperaturaMundialApiView(generics.ListAPIView):
 
 class EmisionesPaisApiView(generics.ListAPIView):
     serializer_class = EmisionesPaisSerializer
-    # filter_backends = [DjangoFilterBackend]
-    # filterset_fields = {
-    #     'anio': ['contains']
-    # }
 
     def get_queryset(self):
         queryset = EmisionesPais.objects.all()
@@ -96,6 +98,11 @@ class EmisionesPaisApiView(generics.ListAPIView):
 
 
 class HuellaGasesTemperaturaMundialApiView(generics.ListAPIView):
+    """
+    Información Mundial General
+
+    Información general,en estructura anidada 
+    """
     serializer_class = HuellaGasesTemperaturaMundialSerializer
 
     def get_queryset(self):
@@ -115,14 +122,30 @@ class PrediccionMundialApiView(generics.ListAPIView):
 
 
 class ConsumoApiView(generics.ListAPIView):
+    """
+    Consumo de energía eléctrica por país
+
+    El consumo está distribuido por tipos de fuentes de energía, código de país según ISO 3166 alfa-3, 
+    otras energías renovables engloba a todas las energías renovables no mencionadas en la estructura JSON.
+    """
     serializer_class = ConsumoSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ('codigo', )
 
     def get_queryset(self):
         queryset = ConsumoPais.objects.all()
         return queryset
 
 class ProduccionApiView(generics.ListAPIView):
+    """
+    Producción de energía eléctrica por país
+
+    La producción está distribuido por tipos de fuentes de energía, código de país según ISO 3166 alfa-3, 
+    otras energías renovables engloba a todas las energías renovables no mencionadas en la estructura JSON.
+    """
     serializer_class = ProduccionSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ('codigo',)
 
     def get_queryset(self):
         queryset = ProduccionPais.objects.all()
